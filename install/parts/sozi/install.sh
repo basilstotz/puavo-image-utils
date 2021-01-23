0197d9f9bddd58aac519c8cdeaa76ba5fe811110
Basil Stotz <basil.stotz@gmail.com>
2021-01-23 21:15:02 +0100

    removed deb

--- install/parts/sozi/install.sh
+++ install/parts/sozi/install.sh
@@ -1,10 +1,15 @@
 #/bin/sh
 cd $(dirname $0)
 
+wget https://github.com/sozi-projects/Sozi/releases/download/v20.05/sozi_20.05.09-1589035558_amd64.deb 
+
 dpkg -i sozi*deb
 
 
 cp sozi.desktop  /usr/share/applications/.
+
+
+rm *deb
 
 
 
