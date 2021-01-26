0502d2daf844f5a7cac6247964dc520dcd450381
root <root@stretch-9ub5iD8Q>
2021-01-26 16:38:20 +0100

    fixed errors

--- puavo-img-patch.sh
+++ puavo-img-patch.sh
@@ -3,19 +3,21 @@
 #you might want to edit these vars
 OSNAME="puavo"
 CLASS="extra"
-SOURCE_IMAGE="./puavo-os-extra-buster-2021-01-25-220739-amd64.img"
+SOURCE_IMAGE="/home/stotz.basil/builder/stretch-builder/images/opinsys/opinsys-os-opinsys-buster-2020-12-09-114058-amd64.img"
+#SOURCE_IMAGE="opinsys-os-opinsys-buster-2020-12-09-114058-amd64.img"
 DATAPATH="./example/example-install/"
 
 if ! test -e ${SOURCE_IMAGE};then
     echo "error: source image not found"
     echo
     echo "       you may want to edit puavo-img-patch.sh"
+fi
 
 #install bins
 cp bin/* /usr/local/bin/.
 
 #call image tool
-puavo-img-tool -datadir ${DATATPATH} -osname ${OSNAME} -class ${CLASS} ${SOURCE_IMAGE}
+puavo-img-tool --datadir ${DATAPATH} --osname ${OSNAME} --class ${CLASS} ${SOURCE_IMAGE}
 
 
 
