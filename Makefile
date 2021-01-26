25e62d0262c404d48b78083a83632abca17db825
root <root@stretch-9ub5iD8Q>
2021-01-26 12:34:54 +0100

    some fixes

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: all
-
 all:
 	@cp bin/* /usr/local/bin/.
 	@./puavo-img-patch.sh
 
+.PHONY: install
 install:
 	@cp bin/* /usr/local/bin/.
--- puavo-img-patch.sh
+++ puavo-img-patch.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+#you might want to edit these vars
 OSNAME="puavo"
 CLASS="extra"
-
 SOURCE_IMAGE="./puavo-os-extra-buster-2021-01-25-220739-amd64.img"
+DATAPATH="example/example-install/"
 
 if ! test -e ${SOURCE_IMAGE};then
     echo "error: source image not found"
@@ -14,7 +15,7 @@ if ! test -e ${SOURCE_IMAGE};then
 cp bin/* /usr/local/bin/.
 
 #call image tool
-puavo-img-tool -o ${OSNAME} -c ${CLASS} ${SOURCE_IMAGE}
+puavo-img-tool -d ${DATATPATH} -o ${OSNAME} -c ${CLASS} ${SOURCE_IMAGE}
 
 
 
